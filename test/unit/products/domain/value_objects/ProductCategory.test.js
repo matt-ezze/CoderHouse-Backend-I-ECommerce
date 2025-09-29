bca3c2da3c705179f85d88327d1919fad17dc6cf
matt-ezze <mati.ezze@gmail.com>
2025-09-29 00:31:20 +0000

    Created unit test for ProductCategory value object

---
+++ test/unit/products/domain/value_objects/ProductCategory.test.js
@@ -0,0 +1,14 @@
+const InvalidProductCategory = require('../../../../../src/products/domain/exceptions/InvalidProductCategory');
+const ProductCategory = require('../../../../../src/products/domain/value_objects/ProductCategory');
+const chai = require('chai');
+const mocha = require('mocha');
+
+mocha.describe('ProductCategory value object', () => {
+	mocha.it('When creating a ProductCategory instance with a length exceeding the maximum, it should throw an InvalidProductCategory exception', () => {
+		// Arrange
+		const invalidCategory = 'a'.repeat(ProductCategory.MAX_LENGTH + 1);
+
+		// Act & Assert
+		chai.expect(() => new ProductCategory(invalidCategory)).to.throw(InvalidProductCategory);
+	});
+});
