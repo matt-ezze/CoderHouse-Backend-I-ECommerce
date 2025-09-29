b1f3f4c5b21e9b4820da6afa942fca3346aa3e28
matt-ezze <mati.ezze@gmail.com>
2025-09-29 00:48:59 +0000

    Created unit test for ProductStock value object

---
+++ test/unit/products/domain/value_objects/ProductStock.test.js
@@ -0,0 +1,17 @@
+const chai = require('chai');
+const mocha = require('mocha');
+const ProductStock = require('../../../../../src/products/domain/value_objects/ProductStock');
+const InvalidProductStockException = require('../../../../../src/products/domain/exceptions/InvalidProductStockException');
+
+mocha.describe('ProductStock value object', () => {
+	mocha.it('When creating a ProductStock instance with a negative value, it should throw an InvalidProductStock exception', () => {
+		// Arrange
+		const invalidStock = -1;
+
+		// Act & Assert
+		chai.expect(() => new ProductStock(invalidStock)).to.throw(
+			InvalidProductStockException,
+			InvalidProductStockException.getProductStockCannotBeNegative().message
+		);
+	});
+});
