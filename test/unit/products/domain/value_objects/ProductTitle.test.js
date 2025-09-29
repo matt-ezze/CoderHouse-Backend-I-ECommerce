876bca57c664595898761100bc60a6e969dc70af
matt-ezze <mati.ezze@gmail.com>
2025-09-29 00:49:22 +0000

    Created unit test for ProductTitle value object

---
+++ test/unit/products/domain/value_objects/ProductTitle.test.js
@@ -0,0 +1,28 @@
+const chai = require('chai');
+const mocha = require('mocha');
+const ProductTitle = require('../../../../../src/products/domain/value_objects/ProductTitle');
+const InvalidProductTitleException = require('../../../../../src/products/domain/exceptions/InvalidProductTitleException');
+
+mocha.describe('ProductTitle value object', () => {
+	mocha.it('When creating a ProductTitle instance with an empty value, it should throw an InvalidProductTitle exception stating that the title cannot be empty', () => {
+		// Arrange
+		const invalidTitle = '';
+
+		// Act & Assert
+		chai.expect(() => new ProductTitle(invalidTitle)).to.throw(
+			InvalidProductTitleException,
+			InvalidProductTitleException.getTitleCannotBeEmpty().message
+		);
+	});
+
+	mocha.it('When creating a ProductTitle instance with a length exceeding the maximum, it should throw an InvalidProductTitle exception', () => {
+		// Arrange
+		const invalidTitle = 'a'.repeat(ProductTitle.MAX_LENGTH + 1);
+
+		// Act & Assert
+		chai.expect(() => new ProductTitle(invalidTitle)).to.throw(
+			InvalidProductTitleException,
+			InvalidProductTitleException.getTitleExceedsMaxLength(ProductTitle.MAX_LENGTH).message
+		);
+	});
+});
