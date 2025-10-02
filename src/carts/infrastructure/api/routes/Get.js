f733779c440d455d940078b9b887919fbc40a12d
matt-ezze <mati.ezze@gmail.com>
2025-10-02 18:52:38 +0000

    Added product details to the response of the get carts api

--- src/carts/infrastructure/api/routes/Get.js
+++ src/carts/infrastructure/api/routes/Get.js
@@ -1,3 +1,6 @@
+const FindProductByIdRequest = require('../../../../products/application/use_cases/find_product_by_id/FindProductByIdRequest');
+const ProductId = require('../../../../products/domain/value_objects/ProductId');
+const FindProductByIdUseCaseFactory = require('../../../../products/infrastructure/factories/FindProductByIdUseCaseFactory');
 const CartNotFoundException = require('../../../application/use_cases/exceptions/CartNotFoundException');
 const FindCartByIdRequest = require('../../../application/use_cases/find_cart_by_id/FindCartByIdRequest');
 const CartId = require('../../../domain/value_objects/CartId');
@@ -9,11 +12,26 @@ const router = express.Router();
 router.get('/carts/:cartId', async (req, res) => {
 	try {
 		const findCartByIdUseCase = FindCartByIdUseCaseFactory.getInstance();
+		const findProductByIdUseCase = FindProductByIdUseCaseFactory.getInstance();
 		const response = await findCartByIdUseCase.execute(
 			new FindCartByIdRequest(new CartId(req.params.cartId))
 		);
+		// Fixme: we create a promise for each product instead of perform one
+		// call to fetch all products in one go.
+		const responses = await Promise.all(response
+			.getCart()
+			.getProducts()
+			.values()
+			.map(product => findProductByIdUseCase.execute(new FindProductByIdRequest(new ProductId(product.getId()))))
+		);
 		res.status(200).json({
-			cart: response.getCart().toJson()
+			cart: {
+				...response.getCart().toJson(),
+				products: Object.fromEntries(responses.map(response => [
+					response.getProduct().getId().getValue(),
+					response.getProduct().toJson()
+				]))
+			}
 		});
 	} catch (error) {
 		if (error instanceof CartNotFoundException) {
