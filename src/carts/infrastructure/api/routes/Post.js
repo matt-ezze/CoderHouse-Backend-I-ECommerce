0d22dfad4d3ba888b6bcb3874ba50dd679e884fd
matt-ezze <mati.ezze@gmail.com>
2025-10-02 19:37:19 +0000

    Added validation to check if the product to be added to the cart exists

--- src/carts/infrastructure/api/routes/Post.js
+++ src/carts/infrastructure/api/routes/Post.js
@@ -1,3 +1,8 @@
+const ProductNotFoundException = require('../../../../products/application/use_cases/exceptions/ProductNotFoundException');
+const FindProductByIdRequest = require('../../../../products/application/use_cases/find_product_by_id/FindProductByIdRequest');
+const ProductId = require('../../../../products/domain/value_objects/ProductId');
+const FindProductByIdUseCaseFactory = require('../../../../products/infrastructure/factories/FindProductByIdUseCaseFactory');
+const CartNotFoundException = require('../../../application/use_cases/exceptions/CartNotFoundException');
 const AddProductToCartUseCaseFactory = require('../../factories/AddProductToCartUseCaseFactory');
 const CreateCartUseCaseFactory = require('../../factories/CreateCartUseCaseFactory');
 const AddProductToCartRequest = require('../../../application/use_cases/add_product_to_cart/AddProductToCartRequest');
@@ -8,9 +13,16 @@ const express = require('express');
 
 const router = express.Router();
 
+const checkProductExists = async (productId) => {
+	const findProductByIdUseCase = FindProductByIdUseCaseFactory.getInstance();
+	await findProductByIdUseCase.execute(new FindProductByIdRequest(productId));
+};
+
 router.post('/carts', async (req, res) => {
 	try {
 		const createCartUseCase = CreateCartUseCaseFactory.getInstance();
+		const request = CreateCartRequest.fromJson(req.body);
+		await Promise.all(request.getProducts().values().map(product => checkProductExists(new ProductId(product.getId()))));
 		const response = await createCartUseCase.execute(CreateCartRequest.fromJson(req.body));
 		res.status(201).json({
 			message: 'Product created successfully',
@@ -20,6 +32,11 @@ router.post('/carts', async (req, res) => {
 		if (error instanceof InvalidJsonSchemaException) {
 			res.status(400).json({
 				error: 'Request schema is invalid',
+				description: error.message
+			});
+		} else if (error instanceof ProductNotFoundException) {
+			res.status(404).json({
+				error: 'Product not found',
 				description: error.message
 			});
 		} else {
@@ -33,6 +50,7 @@ router.post('/carts', async (req, res) => {
 
 router.post('/carts/:cartId/product/:productId', async (req, res) => {
 	try {
+		await checkProductExists(new ProductId(req.params.productId));
 		const addProductToCartUseCase = AddProductToCartUseCaseFactory.getInstance();
 		await addProductToCartUseCase.execute(AddProductToCartRequest.fromJson({
 			cartId: req.params.cartId,
@@ -48,6 +66,16 @@ router.post('/carts/:cartId/product/:productId', async (req, res) => {
 		if (error instanceof InvalidCartPropertyException) {
 			res.status(400).json({
 				error: 'Request schema is invalid',
+				description: error.message
+			});
+		} else if (error instanceof CartNotFoundException) {
+			res.status(404).json({
+				error: 'Cart not found',
+				description: error.message
+			});
+		} else if (error instanceof ProductNotFoundException) {
+			res.status(404).json({
+				error: 'Product not found',
 				description: error.message
 			});
 		} else {
